7d6e6487f7524d06c2a439161108a1f0b06869c6
Iago Luan <iago.luancj@gmail.com>
2025-04-05 19:49:37 -0300

    Correção: Forma de busca das variaveis de ambiente alteradas

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -2,10 +2,10 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
-console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("SUPABASE_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_KEY!
+);
 
 type TaskItem = {
   name: string;
